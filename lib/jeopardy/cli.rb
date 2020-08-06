2d08d187cf0abfba1a9f68d637ddca4b4c3aab9c
Ryna <pinktulips@Hotmail.com>
2020-08-06 09:28:25 -0700

    good flow in main_menu

--- lib/jeopardy/cli.rb
+++ lib/jeopardy/cli.rb
@@ -3,48 +3,31 @@ class CLI
   def start
     self.welcome
     self.main_menu
-    # self.display_random_question
   end
 
   def welcome
-    puts "Welcome to Jeopardy Practice!"
-    # sleep(1)
-    puts "Are you ready to get started?"
+    puts "********************Welcome to Jeopardy Practice!*************************"
     sleep(1)
-    puts "Here are the questions:"
-    self.display_questions_list
-    
+    puts "********************Are you ready to get started?*************************"
+    sleep(1)
+    puts "****************************Let's Practice!*************************"
   end
-
-  # def main_menu
-  #   input = nil
-  #   while input != "exit"
-  #     puts "Please enter a number from 1-200 to pick a specific question you'd like to learn more about. Type the word random to study a random question, or type exit to end"
-  #     self.display_question_info
-  #     case input
-  #     when"exit"
-  #     self.sign_off
-  #     when 'random'
-  #       self.display_random_question
-  #     else
-  #       puts "That is an invalid option, please try again."
-  #     end
-  #   end
-  # end
 
   def main_menu
     input = nil
     while input != "exit"
-      puts "Please enter a number from 1-200 to pick a specific question you'd like to learn more about. Type the word random to study a random question, or type exit to end"
-      self.display_question_info
-      # puts "-------------------------------------------------------------------------"
-      # elsif !self.valid_input?(input)
-      #   puts "That is an invalid option, please try again."
+      puts "Type the word question for jeopardy questions. Type the word random to study a random question, or type exit to end"
+      puts "-------------------------------------------------------------------------"
+      input = gets.strip.downcase
       if input == "random"
         self.display_random_question
-      else 
-        puts "Im sorry that was not valid, please try again"
-        self.main_menu
+      elsif input == "question"
+        self.display_questions_list
+        self.display_question_info
+      elsif input == "exit"
+        self.sign_off
+      else
+        puts "That is an invalid option. Please try again."
       end
     end
   end
@@ -52,6 +35,7 @@ class CLI
 
 
   def display_question_info
+    puts "Please enter a number from 1-200 to pick a specific question you'd like to learn more about."
     input = gets.to_i
     
     puts "You have chosen the question: #{Jeopardy_class.all[input.to_i-1].question}"
@@ -59,7 +43,6 @@ class CLI
     puts "Title: #{Jeopardy_class.all[input.to_i-1].title}"
     puts "Airdate: #{Jeopardy_class.all[input.to_i-1].airdate}"
     puts "--------------------------------------------------------------------------------------"
-    self.main_menu
   end
 
   def display_questions_list
@@ -76,10 +59,6 @@ class CLI
       puts "#{index}: #{obj.question}"
     end
   end
-
-  # def valid_input?(input)
-  #   input.to_i.between?(1, 200) || input == "exit" || input == "random"
-  # end
 
   def sign_off
     puts "Thanks for playing Jeopardy Practice! We wish you all the best! GOOD LUCK!"
